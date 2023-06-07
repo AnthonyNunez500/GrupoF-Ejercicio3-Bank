786c45b68bfa3a3ac035a2118d8c0740c4620776
AnthonyNunez500 <u202119276@upc.edu.pe>
2023-06-06 19:56:18 -0500

    Commit Inicial

---
+++ src/main/java/com/upc/bank/BankSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.upc.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/bank/BankSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
