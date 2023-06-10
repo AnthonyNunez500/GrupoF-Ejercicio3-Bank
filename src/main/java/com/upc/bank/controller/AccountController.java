c4c54c061194764edac8ee3f72afeeb920732e4c
AnthonyNunez500 <u202119276@upc.edu.pe>
2023-06-09 19:25:29 -0500

    Creación de los controllers (con ayuda de Marco Antonio Collantes Artola)

--- src/main/java/com/upc/bank/controller/AccountController.java
+++ src/main/java/com/upc/bank/controller/AccountController.java
@@ -1,8 +1,54 @@
 package com.upc.bank.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.upc.bank.exception.ValidationException;
+import com.upc.bank.model.Account;
+import com.upc.bank.repository.AccountRepository;
+import com.upc.bank.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-
+@RequestMapping("/api/bank/v1")
 public class AccountController {
+    @Autowired
+    private AccountService accountService;
+    private final AccountRepository accountRepository;
+
+    public AccountController(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Transactional(readOnly = true)
+    @GetMapping("/accounts")
+    public ResponseEntity<List<Account>> getAllAccounts() {
+        return new ResponseEntity<List<Account>>(accountRepository.findAll(), HttpStatus.OK);
+    }
+
+    @Transactional
+    @PostMapping("/accounts")
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
+        validateAccount(account);
+        return new ResponseEntity<>(accountService.createAccount(account), HttpStatus.CREATED);
+    }
+
+    private void validateAccount(Account account) {
+        if (account.getNameCustomer() == null || account.getNameCustomer().trim().isEmpty()) {
+            throw new ValidationException("El nombre del cliente debe ser obligatorio");
+        }
+        if (account.getNameCustomer().length() > 30) {
+            throw new ValidationException("“El nombre del cliente no debe exceder los 30 caracteres");
+        }
+        if (account.getNumberAccount() == null || account.getNumberAccount().trim().isEmpty()) {
+            throw new ValidationException("El número de cuenta debe ser obligatorio”");
+        }
+        if (account.getNumberAccount().length() != 13) {
+            throw new ValidationException("“El número de cuenta debe tener una longitud de 13 caracteres");
+
+        }
+    }
 }
---
+++ src/main/java/com/upc/bank/controller/TransactionController.java
@@ -0,0 +1,44 @@
+package com.upc.bank.controller;
+
+import com.upc.bank.exception.ResourceNotFoundException;
+import com.upc.bank.model.Account;
+import com.upc.bank.model.Transaction;
+import com.upc.bank.repository.AccountRepository;
+import com.upc.bank.repository.TransactionRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequestMapping("/api/bank/v1")
+public class TransactionController {
+    private TransactionRepository transactionRepository;
+    private AccountRepository accountRepository;
+    public TransactionController(TransactionRepository transactionRepository, AccountRepository accountRepository){
+        this.transactionRepository=transactionRepository;
+        this.accountRepository=accountRepository;
+    }
+
+    @Transactional(readOnly = true)
+    @GetMapping("/transactions/filterByNameCustomer")
+    public ResponseEntity<List<Transaction>> getAllTransactionsByNameCustomer(@RequestParam(value = "nameCustomer") String nameCustomer){
+        return new ResponseEntity<List<Transaction>>(transactionRepository.filterByNameCustomer(nameCustomer), HttpStatus.OK);
+    }
+
+    @Transactional(readOnly = true)
+    @GetMapping("/transactions/filterByCreateDateRange")
+    public ResponseEntity<List<Transaction>> getAllTransactionsByCreateDateRange(@RequestParam(value = "from") LocalDate from, @RequestParam(value = "to") LocalDate to){
+        return new ResponseEntity<>(transactionRepository.filterByCreateDateRange(from,to),HttpStatus.OK);
+    }
+
+    @Transactional
+    @PostMapping("/accounts/{id}/transactions")
+    public ResponseEntity<Transaction> registerTransaction(@PathVariable(name = "id") Long AccountId, @RequestBody Transaction transaction){
+        Account account=accountRepository.findById(AccountId)
+                .orElseThrow(()->new ResourceNotFoundException("No se encuentra cuenta con el id ingresado"));
+        return new ResponseEntity<Transaction>(transactionRepository.save(transaction),HttpStatus.CREATED);
+    }
+}
